3c129b23874edd860a049a51edbefb3fc1119824
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-10-24 17:40:42 -0500

    added axios and the rest of API integration and JS functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-function showPoem(event) {
-  event.preventDefault();
+function showPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem-response", {
-    strings: "Generating poem for you... ✍🏼",
+    strings: response.data.answer,
     autoStart: true,
     delay: 30,
     cursor: null,
   });
 }
 
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "23ab4btd750623bd6a0096bbc3of4f4f";
+  let context =
+    "You are an uplifting Poem expert and love to write short common poetic forms such as a haiku, limerick, or other very short poems. Generate small poems in basic HTML format. Follow user instructions. Do not include a title or 'html' in the response. Sign the poem with SheCodes AI.";
+  let prompt = `User instructions are: Generate an uplifting poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  new Typewriter("#poem-response", {
+    strings: "Generating a new poem for you ... ✍🏼",
+    autoStart: true,
+    delay: 13,
+    cursor: null,
+  });
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(showPoem);
+}
+
 let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", showPoem);
+poemFormElement.addEventListener("submit", generatePoem);
